b19dcd681f43ead0fbe83ceeb30cf4999b6d90ab
Yash <yyashraj07@gmail.com>
2023-04-02 16:56:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [editMode, setEditMode] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (editMode) {
+      const newTodos = [...todos];
+      newTodos[editIndex] = inputValue;
+      setTodos(newTodos);
+      setEditMode(false);
+      setEditIndex(null);
+    } else {
+      setTodos([...todos, inputValue]);
+    }
+    setInputValue('');
+  };
+
+  const handleEdit = (index) => {
+    setEditMode(true);
+    setEditIndex(index);
+    setInputValue(todos[index]);
+  };
+
+  const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter task"
+        />
+        <button type="submit">{editMode ? 'Update' : 'Add'}</button>
+        {editMode && <button onClick={() => setEditMode(false)}>Cancel</button>}
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li className="mx-3" key={index}>
+            {index+1}.{todo}{' '}
+            <div className="mx-3">
+              <button onClick={() => handleEdit(index)}>Edit</button>{' '}
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
